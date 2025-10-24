27ea7ab43962229cc4da0b3aec7a2a903cbcd14a
fatenmaalem <faten.maalem.partner@decathlon.com>
2025-10-24 13:12:19 +0200

    initial commit

---
+++ src/test/java/com/solocale/AdvertisingCompagn/AdvertisingCompagnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.solocale.AdvertisingCompagn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdvertisingCompagnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
